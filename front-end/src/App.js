db66e7637bd2f4891b4fb75f17057c99f30c47db
Isabella Roman <Isabella Roman>
2023-12-02 21:43:57 -0500

    Basic front end branch

---
+++ front-end/src/App.js
@@ -0,0 +1,17 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>AO3-TRAWLER</h1>
+      </header>
+      <div className="box">
+          <p>test text</p>
+        </div>
+    </div>
+  );
+}
+
+export default App;
