56cef406227126a5e25165bb25d4eca14a0be87f
Isabella Roman <Isabella Roman>
2023-12-03 12:30:38 -0500

    Further developed front-end

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,15 +1,18 @@
 
 import './App.css';
+import GraphGenerator from './components/graphGenerator';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>AO3-TRAWLER</h1>
+        <h1>🦈AO3-TRAWLER🦈</h1>
       </header>
       <div className="box">
-          <p>test text</p>
-        </div>
+        <div className="p">Welcome to AO3 TRAWLER!</div>
+        <div className="p">Choose your 2 Fanction works from the lists below.</div> 
+        <GraphGenerator/>
+      </div>
     </div>
   );
 }
